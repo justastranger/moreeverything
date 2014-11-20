4461c2ae7c9eda4c208c099b5ece091657429136
justastranger <justastrangerlica@gmail.com>
2014-11-19 23:04:02 -0600

    Added support for all EnderIO machines that allowed it with the exception of the Soul Binder(?)

--- src/main/resources/moreEverything/EnderIO.js
+++ src/main/resources/moreEverything/EnderIO.js
@@ -8,9 +8,13 @@ var __eioRecipeOutput = __eIO.machine.recipe.RecipeOutput;
 var __eioRecipe =  __eIO.machine.recipe.Recipe;
 
 var eioNewRecipeOutput;
+var eioNewRecipeOutputFluid;
 var eioNewRecipeInput;
 var eioNewRecipeInputFluid;
 var eioNewRecipe;
+var eioAddCrusherRecipe;
+var eioAddAlloyRecipe;
+var eioAddVatRecipe;
 
 (function(){
     eioNewRecipeOutput = function(stack, chance){
@@ -22,15 +26,22 @@ var eioNewRecipe;
                 throw("newRecipeOutput: stack should be an ItemStack or string, it was a " + stack.getClass());
             }
         }
-        if (!chance) chance = 1;
-        if (chance>1) chance = 1;
-        if (chance<0) chance = 0;
+        if (typeof chance == "undefined") chance = 1;
+        if (chance>1 || chance < 0) chance = 1;
         return new __eioRecipeOutput(stack, chance);
+    };
+
+    eioNewRecipeOutputFluid = function(fluidStack){
+        if(!isJavaClass(fluidStack, __fluidStack)){
+            if(stringOrNumber(fluidStack)) fluidStack = newFluidStack(fluidStack, 1000);
+            else throw("newRecipeInputFluid: fluidStack must be a FluidStack, a string, or a number, it was a " + fluidStack.getClass());
+        }
+        return new __eioRecipeOutput(fluidStack);
     };
 
     eioNewRecipeInput = function(stack, useMeta){
         if(!isJavaClass(stack, __itemStack)){
-            if(typeof stack == "string") {
+            if(typeof stack == "string"){
                 if (stack.indexOf(":") > 0) stack = newItemStack(stack);
                 else stack = getOres(stack)[0];
             } else {
@@ -48,18 +59,37 @@ var eioNewRecipe;
         return new __eioRecipeInput(fluidStack);
     };
     eioNewRecipe = function(input, energy, output){
-        if(!isJavaClass(input, __eioRecipeInput)) input = eioNewRecipeInput(input);
         if(typeof energy != "number") throw("eioNewRecipe: energy must be a number.");
-        if(output instanceof Array) {
-            for(var i = 0; i < output.length; i++) {
+        if(input instanceof Array){
+            for(var i = 0; i < input.length; i++){
+                if(!isJavaClass(input[i], __eioRecipeInput)) input[i] = __eioRecipeInput(input[i]);
+            }
+        } else {
+            if(!isJavaClass(input, __eioRecipeInput)) input = __eioRecipeInput(input);
+        }
+        if(output instanceof Array){
+            for(var i = 0; i < output.length; i++){
                 if(!isJavaClass(output[i], __eioRecipeOutput)) output[i] = eioNewRecipeOutput(output[i]);
             }
         } else {
             if(!isJavaClass(output, __eioRecipeOutput)) output = eioNewRecipeOutput(output);
         }
         return new __eioRecipe(input, energy, output);
-    }
+    };
 
+    eioAddCrusherRecipe = function(recipe, energy, output){
+        if(!isJavaClass(recipe, __eioRecipe)) recipe = eioNewRecipe(recipe, energy, output);
+        __eIO.machine.crusher.CrusherRecipeManager.getInstance().addRecipe(recipe);
+    };
 
+    eioAddAlloyRecipe = function(recipe, energy, output){
+        if(!isJavaClass(recipe, __eioRecipe)) recipe = eioNewRecipe(recipe, energy, output);
+        __eIO.machine.alloy.AlloyRecipeManager.getInstance().addRecipe(recipe);
+    };
+
+    eioAddVatRecipe = function(recipe, energy, output){
+        if(!isJavaClass(recipe, __eioRecipe)) recipe = eioNewRecipe(recipe, energy, output);
+        __eIO.machine.still.VatRecipeManager.getInstance().addRecipe(recipe);
+    };
 
 })();
